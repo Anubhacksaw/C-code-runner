555531bd3a2bf8bcf6792161182e13bb6fd8d811
Anubhab Mukherjee <78078306+Anubhab-ai@users.noreply.github.com>
2022-07-06 22:19:50 +0530

    Update compiler.sh

--- compiler.sh
+++ compiler.sh
@@ -1,5 +1,5 @@
 sf (){
-	vim $a.c
+	subl $a.c
 echo "___Press 1 if you wrote your code___"
 read b 
 if [ "$b" = "1" ]; then
@@ -19,13 +19,31 @@ com (){
 	else
 		break
 	fi
-	
+	echo "___Your Output file___"
+	./$a 
 }
+echo "\033[41;5m         ℂ ℝ𝕌ℕℕ𝔼ℝ          \033[0m"
 echo "Enter the file name: "
 read a 
-touch $a.c
 sf
-echo "___Your Output file___"
-./$a 
-echo "___exiting___"
-echo "Hii from Anubhab"
+while :
+do
+echo " "
+echo "--------------------------"
+echo "1. I want to edit my code"
+echo "2.Exit"
+echo "--------------------------"
+echo " "
+read c
+case $c in
+	1 )
+		sf
+		;;
+	2 )
+		echo "\033[42;5m _____Exiting_____\033[0m"
+		echo "\033[44;5m@Anubhab\033[0m"
+		echo "\033[33mhttps://github.com/Anubhab-ai\033[0m"
+		exit 0
+		;;
+esac
+done
